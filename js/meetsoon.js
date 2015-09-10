1d15143ff363356336ac1bd42d9fab41e1d11c97
Sharojit <sharojitworkbook@gmail.com>
2015-09-11 00:05:16 +0600

    all files

---
+++ js/meetsoon.js
@@ -0,0 +1,61 @@
+	
+	//countdown
+	$(function () {
+		var countDay = new Date();
+		countDay = new Date('December 05, 2014 00:01:00');
+		$('.mate-timer').countdown({until: countDay, 
+		format: 'DHMS',
+		layout: '<div class="clearfix">'+
+								'<div class="col-lg-2 visible-lg"></div>'+
+								'<div class="col-lg-2 col-md-3 col-sm-6 col-xs-12 mate-numbs">{dnnn}<p class="mate-value">{dl}</p></div>'+
+								'<div class="col-lg-2 col-md-3 col-sm-6 col-xs-12 mate-numbs">{hnn}<p class="mate-value">{hl}</p></div>'+
+								'<div class="col-lg-2 col-md-3 col-sm-6 col-xs-12 mate-numbs">{mnn}<p class="mate-value">{ml}</p></div>'+
+								'<div class="col-lg-2 col-md-3 col-sm-6 col-xs-12 mate-numbs">{snn}<p class="mate-value">{sl}</p></div>'+
+								'<div class="col-lg-2 visible-lg"></div>'+
+							'</div>'});
+		$('#year').text(countDay.getFullYear());
+	});
+
+	//Background slider jquery 
+		$(function() {
+			cbpBGSlideshow.init();
+		})
+
+	//scroll bar custom
+		jQuery(document).ready(
+	  function() {  
+		jQuery("html").niceScroll({cursorcolor:"#ffffff"});
+	  }
+	);
+
+	//Preloader
+		//<![CDATA[
+			$(window).load(function() { // makes sure the whole site is loaded
+				$('#status').fadeOut(); // will first fade out the loading animation
+				$('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
+				$('body').delay(350).css({'overflow':'visible'});
+			})
+		//]]>
+		
+		
+	//Background Video
+	$(document).ready(function() {
+
+		$('#video').videoBG({
+			mp4:'video/heart.mp4',
+			ogv:'video/heart.ogv',
+			webm:'video/heart.webm',
+			poster:'video/www.sharojit.com.png',
+			scale:true,
+			zIndex:0
+		});	
+			
+	})
+	
+	//Scroll to 
+		function scrollToBottom() {
+		$('html, body').animate({scrollTop:$(document).height()}, 'slow');
+	}
+	function scrollToTop() {
+		$('html, body').animate({scrollTop:0}, 'slow');
+	}
---
+++ php/contactthanks.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Meetsoon - Coming Soon Thanks Template</title>
+
+    <!-- Bootstrap -->
+    <link href="../css/bootstrap.css" rel="stylesheet" media="screen">
+
+	<!-- Fontawsome -->
+    <link href="../css/fonts/fontawesome/css/font-awesome.css" rel="stylesheet" media="screen">
+	<!-- Meetsoon's Style sheet -->
+    <link href="thanks.css" rel="stylesheet" media="screen">
+	
+	<!--[if lt IE 9]>
+    <script src="../js/html5shiv.js"></script>
+    <script src="../js/respond.min.js"></script>
+    <![endif]-->  
+	
+    <!-- fav and touch icons -->
+    <link rel="shortcut icon" href="../img/ico/favicon.ico">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="../img/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="../img/ico/apple-touch-icon-114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="../img/ico/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="../img/ico/apple-touch-icon-57-precomposed.png">
+</head>
+<body>
+	<div class="container">
+		<!-- Header -->
+		<header class="clearfix">
+					<!-- Keep it blank -->
+		</header>
+		<!-- /header -->
+
+		<!-- Body Sections -->
+		<section class="clearfix text-center">
+			<div class="row">
+				<!-- This is some text you can put -->
+				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 clearfix">
+					<i class="fa fa-leaf fa-5x logo-icon"></i>
+					<h1 class="logo-text">Meet Soon</h1>
+					<h2>Thank you for contacting us.</h2>
+					<p>We will be in touch with you very soon.</p>
+				</div>
+				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 header-social clearfix">
+					<!-- Social Icon -->
+					<ul class="list-inline">
+						<li><a href="#" title="Like us on Facebook"><i class="fa fa-facebook facebook"></i></a></li>
+						<li><a href="#" title="follow us on twitter"><i class="fa fa-twitter twitter"></i></a></li>
+						<li><a href="#" title="Link us on Linkedin"><i class="fa fa-linkedin linkedin"></i></a></li>
+						<li><a href="#" title="add us on Google Plus"><i class="fa fa-google-plus google"></i></a></li>
+						<li><a href="#" title="Like us on Tumblr"><i class="fa fa-tumblr tumblr"></i></a></li>
+					</ul>
+				</div>
+				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 header-bottom-icon clearfix">
+					<!-- Header Go Down Icon -->
+					<a href="#" title="hi there"><i class="fa fa-chevron-down mate-down"></i></a>
+				</div>
+				<!-- This is the back to home button --->
+				<a href="../../index.html" class="btn mate-btn" title="Let's go for home....:)">Back to Home</a>				
+			</div>
+		</section>
+		<!-- /Body Sections -->
+		
+		<!-- Footer -->
+		<footer class="clearfix">
+			<!-- Keep it blank -->
+		</footer>
+		<!-- /Footer -->
+	</div>
+	
+	<!--- Slider --->
+			<div class="main">
+				<ul id="cbp-bislideshow" class="cbp-bislideshow">
+					<li><img src="../img/background/1.jpg" alt="image01"/></li>
+					<li><img src="../img/background/2.jpg" alt="image02"/></li>
+					<li><img src="../img/background/3.jpg" alt="image03"/></li>
+					<li><img src="../img/background/4.jpg" alt="image04"/></li>
+				</ul>
+			</div>
+
+	<!-- required JS files -->
+    <script src="../js/jquery.js"></script>
+	<script src="../vendor/jquery.imagesloaded.min.js"></script>
+	<script src="../vendor/cbpBGSlideshow.min.js"></script>
+	<script src="../vendor/modernizr.custom.js"></script>
+	<script type="text/javascript">
+	<!-- Backgroound slider -->
+	//Background slider jquery 
+		$(function() {
+			cbpBGSlideshow.init();
+		})
+	</script> 
+</body>
+</html>
